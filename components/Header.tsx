82934c6be5a2587a234059fd980186a9ab0f336c
safa <safabelhouch@gmail.com>
2025-10-19 21:59:32 +0100

    Implement Header

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "./NavItems";
+import UserDropDown from "./UserDropDown";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Signalist Logo"
+            className="h-8 w-auto cursor-pointer"
+            width={25}
+            height={25}
+          />
+        </Link>
+        <nav className="hidden sm:block">
+            {/* NavItems */}
+            <NavItems />
+        </nav>
+
+        <UserDropDown />
+
+
+
+      </div>
+    </header>
+  );
+};
+
+export default Header;
